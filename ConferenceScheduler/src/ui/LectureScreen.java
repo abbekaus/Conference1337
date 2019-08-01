2de2b3576225cefa968d1dcfc3b43f64bb1be24e
abbekaus <johanmattsson91@gmail.com>
2019-08-01 21:36:56 +0200

    Added done button to lecture screen

--- ConferenceScheduler/src/ui/LectureScreen.java
+++ ConferenceScheduler/src/ui/LectureScreen.java
@@ -5,8 +5,6 @@ import java.util.ArrayList;
 import conferenceApp.Attendee;
 import conferenceApp.Conference;
 import conferenceApp.Event;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
@@ -15,8 +13,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
-import javafx.scene.control.Toggle;
-import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
@@ -24,6 +20,7 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
 
 public class LectureScreen {
 
@@ -34,14 +31,14 @@ public class LectureScreen {
 		this.currentAttendee = att;
 		this.conference = conf;
 	}
-	
+
 	public void drawScreen() {
 		GridPane grid = new GridPane();
 		grid.setAlignment(Pos.CENTER);
 		grid.setHgap(10);
 		grid.setVgap(10);
 		grid.setPadding(new Insets(25, 25, 25, 25));
-		
+
 		Text amTitle = new Text("Choose AM Lecture:");
 		amTitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
 		grid.add(amTitle, 0, 0, 2, 1);
@@ -54,37 +51,45 @@ public class LectureScreen {
 		lunchTitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
 		grid.add(lunchTitle, 0, 8, 2, 1);
 		addOptions(conference.getLunchEvents(), grid, 9);
-		
+		Button doneButton = new Button("Done");
+		HBox doneHbButton = new HBox(10);
+		doneHbButton.setAlignment(Pos.BOTTOM_RIGHT);
+		doneHbButton.getChildren().add(doneButton);
+		grid.add(doneButton, 0, 12, 2, 1);
+
 		Stage stage = new Stage();
-		stage.setTitle("My New Stage Title");
+		stage.setTitle("Available Lectures");
 		stage.setScene(new Scene(grid, 450, 450));
 		stage.show();
+
+		doneButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				conference.saveAttendees();
+				stage.close();
+			}
+		});
 	}
-	
+
 	private void addOptions(ArrayList<Event> events, GridPane grid, int offset) {
 		int nbrOfEvents = events.size();
 		final ToggleGroup toggleGroup = new ToggleGroup();
 		for (int i = 0; i < nbrOfEvents; i++) {
 			Event currentEvent = events.get(i);
 			Label label = new Label(currentEvent.getEventID() + ": " + currentEvent.getDescription());
-			grid.add(label, 0, i+offset);
+			grid.add(label, 0, i + offset);
 			RadioButton button = new RadioButton();
 			HBox hbBtn = new HBox(10);
 			hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
 			hbBtn.getChildren().add(button);
-			grid.add(button, 1, i+offset);
+			grid.add(button, 1, i + offset);
 			button.setToggleGroup(toggleGroup);
 			button.setOnAction(new EventHandler<ActionEvent>() {
 				@Override
 				public void handle(ActionEvent event) {
-					currentAttendee.addEvent(currentEvent);
-					System.out.println("Deltagare: " + conference.getNbrOfAttendees());
-					
+					currentAttendee.signUp(currentEvent);
 				}
 			});
 		}
-		
-
 	}
-
 }
